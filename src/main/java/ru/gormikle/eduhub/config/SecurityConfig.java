ecd7ecc8a8d7f9f7071209f8f4542029e36d7c21
Mikhail Goryunov <m.goryunov@smartdata.dev>
2024-05-22 03:36:14 +0800

    Правка CORS

--- src/main/java/ru/gormikle/eduhub/config/SecurityConfig.java
+++ src/main/java/ru/gormikle/eduhub/config/SecurityConfig.java
@@ -43,7 +43,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     var corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOriginPatterns(List.of("http://158.160.115.101:3000"));
+                    corsConfiguration.setAllowedOriginPatterns(List.of(
+                            "https://158.160.115.101",
+                            "https://158.160.115.101:443"
+                    ));
                     corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                     corsConfiguration.setAllowedHeaders(List.of("*"));
                     corsConfiguration.setAllowCredentials(true);
